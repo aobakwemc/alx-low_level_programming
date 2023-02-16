8f0e832f2d42c3aac04696cabab56b267248aade
Aobakwe <aobakwemc@gmail.com>
2023-02-16 23:36:29 +0000

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /*
- * main - prints Programming is like building a multilingual puzzle
+ * main - prints a string
  * returns - 0 upon successful run. Non 0 value if failed
  */
 
