cb95ee7f08eecb63f57f96de9a55fd9a264c4020
Aobakwe <aobakwemc@gmail.com>
2023-02-21 23:53:00 +0000

    0. _putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,20 @@
+/**
+ * This file serves as a library for function prototypes.
+ * Task: 0x02. C - Functions, nested loops
+ * To make use of function prototypes from this file,
+ * include this file as a library within the working
+ * file (source code).
+ */
+int main(void);
+int _putchar(int);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
