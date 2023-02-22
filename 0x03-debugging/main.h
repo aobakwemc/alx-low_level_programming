0956843028f57fef8bd493d7c52a039628532bda
Aobakwe <aobakwemc@gmail.com>
2023-02-22 20:51:45 +0000

    0. Multiple mains

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,15 @@
+/**
+ * This file serves as a header for
+ * all function prototypes within the
+ * 0x03-debugging directory.
+ */
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+
+#endif /* MAIN_H */
