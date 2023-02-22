905045c7bea7b24e1540d81d2a91444a6b8bc862
Aobakwe <aobakwemc@gmail.com>
2023-02-22 01:58:12 +0000

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * @c1: first int parameter
+ * @c2: second int parameter
+ * Return: result of addition
+ */
+int add(int c1, int c2)
+{
+	return (c1 + c2);
+}
