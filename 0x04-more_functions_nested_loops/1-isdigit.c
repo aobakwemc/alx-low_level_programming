4a6dd0fe9ab39525e25d7f6961a8406c675d6edb
Aobakwe <aobakwemc@gmail.com>
2023-02-23 15:39:18 +0000

    1. isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+/**
+ * _isdigit - checks for a digit (0 through 9)
+ * @c: accepts char argument as int equivalent of ASCII value
+ * Return: 1 if c is digit, 0 if otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
