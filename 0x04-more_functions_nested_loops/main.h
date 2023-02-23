39be240260b1d65b915f51abb934e197cfcf10c1
Aobakwe <aobakwemc@gmail.com>
2023-02-23 15:28:34 +0000

    0. isupper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,24 @@
+/**
+ * This is a header file that contains
+ * function declarations for all tasks
+ * under the 0x04-more_functions_nested_loops
+ * directory.
+ * It will be used and included as a library.
+ *
+ * #ifndef and #define are known as header guards,
+ * Their primary purpose is to prevent header files
+ * from being included multiple times.
+ * #endif determines if the preprocessing of #if,
+ * #ifdef, or #ifndef has been completed successfully.
+ * 
+ * <stdio.h> is included as well as some functions
+ * will make use of stdin and stdout
+ */
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+int _putchar(char c);
+int _isupper(int c);
+
+#endif /* MAIN_H */
