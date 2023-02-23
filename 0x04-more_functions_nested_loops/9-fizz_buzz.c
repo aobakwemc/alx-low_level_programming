7009c8213411e659075d7141f412a57d23829715
Aobakwe <aobakwemc@gmail.com>
2023-02-23 22:56:05 +0000

    9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,14 +14,25 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0)
+		{
 			printf("Fizz");
-		if (i % 5 == 0)
+		}
+		else if (i % 5 == 0)
+		{
 			printf("Buzz");
-		if (i % 3 == 0 && i % 5 == 0)
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
 			printf("FizzBuzz");
-		if (i != 100)
-			printf(" ");
+		}
+		else
+		{
 		printf("%d", i);
+		}
+		if (i != 100)
+		{
+			printf(" ");
+		}
 	}
 	printf("\n");
 
