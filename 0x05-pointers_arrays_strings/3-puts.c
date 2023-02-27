9ebf785b3f8b8ede6c6273d8a1fd67fc888fa795
Aobakwe <aobakwemc@gmail.com>
2023-02-27 21:14:16 +0000

    3. I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,10 @@
+/**
+ * _puts - prints a string, followed by a new line, to stdout.
+ * @str: string to be output to stdout
+ */
+#include"main.h"
+
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
