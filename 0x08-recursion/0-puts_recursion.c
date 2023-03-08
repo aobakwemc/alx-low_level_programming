7f76d585564b57fca936b7ceb03db92a6945b56d
Aobakwe <aobakwemc@gmail.com>
2023-03-08 19:22:55 +0000

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+/**
+ * _puts_recursion - prints a string,
+ * followed by a new line.
+ * @s: string to print.
+ */
+#include "main.h"
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		_putchar('\n');
+}
