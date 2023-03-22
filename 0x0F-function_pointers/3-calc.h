47f2dc7b554ccaee0a2f757fcb5a75df2ae62940
Aobakwe <aobakwemc@gmail.com>
2023-03-22 22:33:08 +0000

    A program that performs simple operations.

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -2,21 +2,20 @@
 #define CALC_H
 
 /**
+ * struct op - Struct op
+ * @op: The operator
+ * @f: The function associated
+ */
+
+/**
  * File: 3-calc.h
  * Description: Header file with structs + prototypes
  * used by 3-main.c
  */
-
-/**
- *  * struct op - Struct op
- *   *
- *    * @op: The operator
- *     * @f: The function associated
- *      */
 typedef struct op
 {
-	    char *op;
-	        int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 
 int op_add(int a, int b);
