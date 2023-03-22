1f576376f56e417d7c03e4769bf963b846036416
Aobakwe <aobakwemc@gmail.com>
2023-03-22 21:28:17 +0000

    Write a function that prints a name.

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,15 @@
 /**
- * print_name - a function that prints a name.
- * @name: name to print
- * @f: pointer to function that prints a name.
- * Return: void
+ * print_name - prints a name.
+ * @name: name to be printed
+ * @f: pointer to function
+ * Return: nothing
  */
+
+#include <stdio.h>
 #include "function_pointers.h"
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
-		f(name);
+	if (!name || !f)
+		return;
+	f(name);
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,17 +1,7 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-#include <stdio.h>
-#include <stdlib.h>
-/**
- * File: function_pointers.h
- * Include guard name taken from header file
- * name - as convention, in uppercase.
- * Description: Header file containing function
- * prototypes for tasks in the 0x0F-function_pointers
- * directory.
- */
-
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
