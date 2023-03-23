7fdb4f36ac40440e77dc83cd3d3922ab7b705b10
Aobakwe <aobakwemc@gmail.com>
2023-03-23 20:16:23 +0000

    Write a function that prints anything.

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,6 +6,18 @@
 #include <stddef.h>
 #include <stdarg.h>
 
+/**
+ * struct print - print type with corresponding print function
+ * @type: indicated the data type
+ * @func: function pointer to function to print data type
+ */
+
+typedef struct print
+{
+	char *type;
+	void (*func)(va_list arg);
+} print_t;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
